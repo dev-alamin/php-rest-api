2786774f7007493e9401009b63b6ac8ac0cd5fc8
dev-alamin <hmalaminmb4@gmail.com>
2022-09-04 20:23:04 +0600

    Initial commit

---
+++ Config/Database.php
@@ -0,0 +1,30 @@
+<?php 
+
+/**
+ * Database class
+ * @return Object
+ * main class
+ */
+
+ class Database{
+    private $host = 'localhost';
+    private $dbname = 'rest';
+    private $username = 'root';
+    private $password = '';
+    private $conn;
+
+    // DB Connect
+    public function connect()
+    {
+        $this->conn = null;
+
+        try{
+            $this->conn = new PDO( 'mysql:host='. $this->host . ';dbname=' . $this->dbname, $this->username, $this->password );
+            $this->conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }catch(PDOException $e){
+            echo 'Connection error: ' . $e->getMessage();
+        }
+
+        return $this->conn;
+    }
+ }
